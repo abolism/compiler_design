aa848d26483ecc4b836a5da1852a1a6c9bf1c2a1
abolfazl <eshagh.abolfazl@gmail.com>
2023-04-07 22:48:14 +0430

    create repository

---
+++ compiler.py
@@ -0,0 +1,45 @@
+from scanner import Scanner
+from dfa import getDfa
+if __name__ == "__main__":
+    dfa = getDfa()
+    scanner = Scanner(dfa, f"input.txt")
+    ignore = ['WHITESPACE', 'COMMENT']
+    tokens = ""
+    errors = ""
+    hold_line = 0
+    while True:
+        try:
+            line, type, token, message = scanner.get_next_token()
+            # print(line, type, token, message)
+            if type not in ignore and type != 'ERROR':
+                if line != hold_line:
+                    tokens += f"\n {line} {type} {token}"
+                    hold_line = line
+                else:
+                    tokens += f"{type} {token}"
+
+            elif type == 'ERROR':
+                if line != hold_line:
+                    errors += f"\n {line} {token} {message}"
+                    hold_line = line
+                else:
+                    errors += f"{token} {message}"
+
+        except Exception as exp:
+            break
+
+    if errors == "":
+        errors = "There is no lexical error."
+    with open("tokens.txt", "w") as f:
+        f.write(tokens)
+    with open("lexical_errors.txt", "w") as f:
+        f.write(errors)
+
+    print(tokens)
+    print(errors)
+
+    with open("symbol_table.txt", "w") as f:
+        for i, token in enumerate(scanner.symbol_table):
+            f.write(f"{i} {token}")
+
+    print(scanner.symbol_table)
---
+++ scanner.py
@@ -0,0 +1,61 @@
+class Scanner:
+    def __init__(self, dfa, input_file):
+        self.dfa = dfa
+        self.input_file = input_file
+        self.file = open(input_file, "r")
+        self.symbol_table = []
+        self.current_char = self.file.read(1)
+        self.current_index = 0
+        self.current_line = 1
+        # self.file.close()
+
+    def get_next_token(self):
+        if self.current_char == '':
+            self.file.close()
+            raise Exception("End of file reached!")
+
+        state = self.dfa.start_state
+        token = ""
+        while True:
+            state = self.dfa.get_next_state(state, self.current_char)
+            if state is None:
+                break
+            token = token + self.current_char
+            self.current_char = self.file.read(1)
+            if self.current_char == '\n':
+                self.current_line = self.current_line + 1
+
+        if state in self.dfa.final_states:
+            if state in self.dfa.accepting_states:
+                if state == 'ID_KEYWORD':
+                    if token in self.dfa.keywords:
+                        return self.current_line, 'KEYWORD', token, None
+                    else:
+                        if token not in self.symbol_table:
+                            self.symbol_table.append(token)
+                        return self.current_line, 'ID', token, None
+                elif state == 'NUM':
+                    return self.current_line, 'NUM', token, None
+                elif state == 'SYMBOL':
+                    return self.current_line, 'SYMBOL', token, None
+                elif state == 'COMMENT':
+                    return self.current_line, 'COMMENT', token, None
+            else:
+                return self.current_line, 'WHITESPACE', token, None
+        else:
+            return self.current_line, 'ERROR', token, 'Invalid token!'
+
+    def get_symbol_table(self):
+        return self.symbol_table
+
+    def get_current_line(self):
+        return self.current_line
+
+    def get_current_char(self):
+        return self.current_char
+
+    def get_input_file(self):
+        return self.input_file
+
+    def get_dfa(self):
+        return self.dfa
